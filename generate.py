4a4986c277b87047663794ab669bd775b925a363
Oliver Staudt <oliver.staudt@deutschebahn.com>
2022-10-28 14:21:21 +0200

    added csv export

--- generate.py
+++ generate.py
@@ -6,11 +6,62 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from pathlib import Path 
 import yaml
-import sys
 import glob
 import os
 import math
 import plotext as plt
+import math
+
+def round_hours(hours):
+    full_hours = math.floor(hours)
+    part = hours % 1
+    if part == 0 or part == 0.25 or part == 0.5 or part == 0.75:
+        return hours
+    if part < 1 and part > 0.75:
+        full_hours = full_hours + 1 
+        part = 0
+    if part < 0.75 and part > 0.5:
+        part = 0.75
+    if part < 0.5 and part > 0.25:
+        part = 0.5
+    if part > 0 and part < 0.25:        
+        part = 0.25
+    return full_hours+part
+
+def csv_export(df):
+    fileName, elements = None,{}
+    with open('config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        if config["csv"]:
+            if config["csv"]["fileName"]:
+                fileName = config["csv"]["fileName"]
+            else: return None
+            if config["csv"]["elements"]:
+                elements = config["csv"]["elements"]
+            else: return None
+        else:
+            return None
+
+    df2 = df[df.Projekt.isin(elements.keys())]
+    df2["element"] = df2["Projekt"].map(elements)
+    
+    df2 = df2.rename(columns={"Datum": "date"})
+    df2 = df2.rename(columns={"Arbeitszeit": "hours"})
+    df2.sort_values(by='date', inplace=True)
+    #df2['date'] = df2['date'].dt.strftime('%d.%m.%Y')
+
+    df_grp = df2.sort_values(['date','element'],ascending=False).groupby(['date','element']).sum()
+    df_grp["hours"] = df_grp["hours"].map(round_hours)
+    df_grp["comment"] = ""
+    df_grp["aktivitätencode"] = ""
+    #df_grp['date'] = df_grp['date'].dt.strftime('%d.%m.%Y')
+    df_grp.to_csv(fileName,sep=';')
+
+    fd = pd.read_csv(fileName, sep=";")
+    fd["date"] = pd.to_datetime(fd["date"])
+    fd['date'] = fd['date'].dt.strftime('%d.%m.%Y')
+    fd.to_csv(fileName,sep=';', columns=["date","element","hours","comment","aktivitätencode"], header=True, index=False)
+    pass
 
 def format_excel(new_file, sheet_name):
     # Format excel
@@ -73,61 +124,70 @@ def visual(percent):
         rval += "#"
     return rval
 
+def read_excel(filename):
+    df = pd.read_excel(filename, sheet_name="Timesheet")
+    df.drop('Startzeit',axis='columns', inplace=True)
+    df.drop('Endzeit',axis='columns', inplace=True)
+    df.drop('Nummer',axis='columns', inplace=True)
+    df.drop('Dauer',axis='columns', inplace=True)
+    df.drop('Beschreibung',axis='columns', inplace=True)
+    df = df.rename(columns={"Dauer (rel.)": "Arbeitszeit"})
+    return df
 
-list_of_files = glob.glob('./T*.xls') # * means all if need specific format then *.csv
-latest_file = Path(max(list_of_files, key=os.path.getctime))
-print(f"Benutzte Daten: {latest_file}")
+def main():
+    list_of_files = glob.glob('./T*.xls') # * means all if need specific format then *.csv
+    latest_file = Path(max(list_of_files, key=os.path.getctime))
+    print(f"Benutzte Daten: {latest_file}")
 
-df = pd.read_excel(latest_file, sheet_name="Timesheet")
-df.drop('Startzeit',axis='columns', inplace=True)
-df.drop('Endzeit',axis='columns', inplace=True)
-df.drop('Nummer',axis='columns', inplace=True)
-df.drop('Beschreibung',axis='columns', inplace=True)
-df = df.rename(columns={"Dauer (rel.)": "Arbeitszeit"})
+    df = read_excel(latest_file)
 
-df_excel = df.groupby(['Datum','Projekt']).sum()
-sheet_name = "Timmesheet"
-new_file = "./Calced_timesheet.xlsx"
-with pd.ExcelWriter(new_file, mode='w', engine='openpyxl') as writer:  
-    df_excel.to_excel(writer, sheet_name= sheet_name)
+    df_excel = df.groupby(['Datum','Projekt']).sum()
+    sheet_name = "Timmesheet"
+    new_file = "./Calced_timesheet.xlsx"
+    with pd.ExcelWriter(new_file, mode='w', engine='openpyxl') as writer:  
+        df_excel.to_excel(writer, sheet_name= sheet_name)
 
-#Information for cli
-print("\n#################\nWochen:\n")
+    csv_export(df)
 
-df['Woche'] = df['Datum'].dt.isocalendar().week
+    #Information for cli
+    print("\n#################\nWochen:\n")
 
-df_week = df.groupby(['Woche']).agg({"Arbeitszeit": np.sum, "Datum": pd.Series.nunique})
-df_week['Soll_Zeit']= df_week['Datum'] * 7.8
-df_week['Diff'] = df_week['Arbeitszeit'] - df_week['Soll_Zeit']
-print(df_week)
-with pd.ExcelWriter(new_file, mode='a', engine='openpyxl') as writer:  
-    df_week.to_excel(writer, sheet_name= "Zeit pro Woche")
-print(f"\n\tSumme der Differenz: {'{0:.2f}h'.format(df_week['Diff'].sum())}")
+    df['Woche'] = df['Datum'].dt.isocalendar().week
+
+    df_week = df.groupby(['Woche']).agg({"Arbeitszeit": np.sum, "Datum": pd.Series.nunique})
+    df_week['Soll_Zeit']= df_week['Datum'] * 7.8
+    df_week['Diff'] = df_week['Arbeitszeit'] - df_week['Soll_Zeit']
+    print(df_week)
+    with pd.ExcelWriter(new_file, mode='a', engine='openpyxl') as writer:  
+        df_week.to_excel(writer, sheet_name= "Zeit pro Woche")
+    print(f"\n\tSumme der Differenz: {'{0:.2f}h'.format(df_week['Diff'].sum())}")
 
 
 
-df_prj = df.groupby(['Projekt']).sum()
-df_prj['percent']  = (df_prj['Arbeitszeit'] / df_prj['Arbeitszeit'].sum()) * 100
-df_prj['percent_visual'] = df_prj.apply(lambda row : visual(row['percent']), axis = 1)
-df_prj['percent'] = pd.Series(["{0:.2f}%".format(val) for val in df_prj['percent']], index = df_prj.index)
+    df_prj = df.groupby(['Projekt']).sum()
+    df_prj['percent']  = (df_prj['Arbeitszeit'] / df_prj['Arbeitszeit'].sum()) * 100
+    df_prj['percent_visual'] = df_prj.apply(lambda row : visual(row['percent']), axis = 1)
+    df_prj['percent'] = pd.Series(["{0:.2f}%".format(val) for val in df_prj['percent']], index = df_prj.index)
 
 
-df_perWeek = df.groupby(['Woche','Projekt']).agg({'Arbeitszeit':['sum']})
-df_perWeek['percent'] = df_perWeek.Arbeitszeit['sum']/39 *100
-df_perWeek['percent'] = pd.Series(["{0:.0f}%".format(val) for val in df_perWeek['percent']], index = df_perWeek.index)
+    df_perWeek = df.groupby(['Woche','Projekt']).agg({'Arbeitszeit':['sum']})
+    df_perWeek['percent'] = df_perWeek.Arbeitszeit['sum']/39 *100
+    df_perWeek['percent'] = pd.Series(["{0:.0f}%".format(val) for val in df_perWeek['percent']], index = df_perWeek.index)
 
-sheet_name2 = "ProjectPerWeek"
-with pd.ExcelWriter(new_file, mode='a', engine='openpyxl') as writer:  
-    df_perWeek.to_excel(writer, sheet_name= sheet_name2)
+    sheet_name2 = "ProjectPerWeek"
+    with pd.ExcelWriter(new_file, mode='a', engine='openpyxl') as writer:  
+        df_perWeek.to_excel(writer, sheet_name= sheet_name2)
 
-df_prj.drop('Woche',axis='columns', inplace=True)
-print("\nVerteilung:")
-print(df_prj)
-print_plotext(df_prj.index.array.to_numpy(),df_prj['Arbeitszeit'].to_numpy())
+    df_prj.drop('Woche',axis='columns', inplace=True)
+    print("\nVerteilung:")
+    print(df_prj)
+    print_plotext(df_prj.index.array.to_numpy(),df_prj['Arbeitszeit'].to_numpy())
 
-print(f"Geschrieben: {new_file}")
+    print(f"Geschrieben: {new_file}")
 
-format_excel(new_file, sheet_name2)
-format_excel(new_file, sheet_name)
+    format_excel(new_file, sheet_name2)
+    format_excel(new_file, sheet_name)
 
+if __name__ == '__main__':
+    main()
    
